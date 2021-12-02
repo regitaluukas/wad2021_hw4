832fcbc34b3e5a8730bab77a1d8631533e928f77
regitaluukas <regita.luukas@gmail.com>
2021-12-02 16:11:35 +0200

    handling requests improve

--- server.js
+++ server.js
@@ -20,47 +20,64 @@ app.get('/posts', async(req, res) => {
     const posts = await pool.query(
     "SELECT * FROM posts"
     );
-    res.json(posts.rows);
+    res.render('posts', { posts: posts.rows });
     } catch (err) {
     console.error(err.message);
     }
 });
 
-app.get('/posts/:id', async(req, res) => {
+app.get('/singlepost/:id', async(req, res) => {
     try {
-    console.log("get a post request has arrived");
-    const {id} = req.params
+    const id = req.params.id;
+    console.log(req.params.id);
+    console.log("get a single post request has arrived");
     const posts = await pool.query(
     "SELECT * FROM posts WHERE id = $1", [id]
     );
-    res.json(posts.rows[0]);
-    } catch (err) {
-    console.error(err.message);
- }
-});
-
-app.post('/posts/', async(req, res) => {
-    try {
-    console.log("a post request has arrived");
-    const post = req.body;
-    const newpost = await pool.query(
-    "INSERT INTO posts(title, body, urllink) posts ($1, $2, $3) RETURNING*", [post.title, post.body, post.urllink]
-    );
-    res.json( newpost );
+    res.render('singlepost', { posts: posts.rows[0] });
     } catch (err) {
     console.error(err.message);
     }
-});
+   });
 
-app.delete('/posts/:id', async(req, res) => {
+
+app.get('/posts/:id', async(req, res) => {
     try {
-    const {id} = req.params;
+    const { id } = req.params;
+    console.log("get a post request has arrived");
+    const Apost = await pool.query(
+    "SELECT * FROM posts WHERE id = $1", [id]
+    );
+    res.json(Apost.rows[0]);
+    } catch (err) {
+    console.error(err.message);
+    }
+   });
+   
+   app.delete('/posts/:id', async(req, res) => {
+    try {
+    console.log(req.params);
+    const { id } = req.params;
     const post = req.body;
     console.log("delete a post request has arrived");
-    const deletepost = await pool.query("DELETE FROM posts WHERE id = $1", [id]
+    const deletepost = await pool.query(
+    "DELETE FROM posts WHERE id = $1", [id]
     );
-    res.json(post);
+    res.redirect('posts');
     } catch (err) {
     console.error(err.message);
     }
-});
+   });
+
+   app.post('/posts', async(req, res) => {
+    try {
+    const post = req.body;
+    console.log(post);
+    const newpost = await pool.query(
+    "INSERT INTO nodetable(title, body, urllink) values ($1, $2, $3)RETURNING*", [post.title, post.body, post.urllink]
+    );
+    res.redirect('posts');
+    } catch (err) {
+    console.error(err.message)
+    }
+   });
