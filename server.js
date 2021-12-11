8d0248feda50bae53f4329b17a3877f7e85ba0aa
Kadi-RiinToomoja <kadiriin.toomoja@gmail.com>
2021-12-11 15:58:04 +0200

    posts will now be shown so that newer ones are first

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.get(['/posts', '/'], async(req, res) => {
     try {
     console.log("get posts request has arrived");
     const posts = await pool.query(
-    "SELECT * FROM posts ORDER BY id"
+    "SELECT * FROM posts ORDER BY id DESC"
     );
     res.render('posts', { posts: posts.rows });
     } catch (err) {
